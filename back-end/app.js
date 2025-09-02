bf2c35ddbefabd43c49173a220c1ed09a7d1888a
adrianfachi <adrianfachi5@gmail.com>
2025-09-02 11:32:21 -0300

    teste

--- back-end/app.js
+++ back-end/app.js
@@ -5,7 +5,7 @@ import cors from "cors"
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: "https://grupo-de-estudos-sprint2-api-de-ava.vercel.app/"
+    origin: "*"
 }))
 app.use("/", filmeRouter)
 
