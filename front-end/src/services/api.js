614fafc7c6db078671049596cdb049614174ea71
adrianfachi <adrianfachi5@gmail.com>
2025-09-02 11:46:38 -0300

    mudando novamente o link da api

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "grupo-de-estudos-sprint2-api-de-ava-sigma.vercel.app"
+    baseURL: "grupo-de-estudos-sprint2-api-de-ava-nine.vercel.app"
 })
 
 export default api
