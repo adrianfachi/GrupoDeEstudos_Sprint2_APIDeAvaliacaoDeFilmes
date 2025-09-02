de9fc86255265df3d176f3d3dc103a33dcca35be
adrianfachi <adrianfachi5@gmail.com>
2025-09-02 14:27:26 -0300

    mudando o link da api

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = "https://api.render.com/deploy/srv-d2r6ifadbo4c73ctcq00?key=3LdMe2vV5_s"
+const API_URL = "https://grupodeestudos-sprint2.onrender.com/"
 
 const api = axios.create({
     baseURL: API_URL
